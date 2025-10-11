d7596d90c939ee5b3912e37e211050390b144e2a
lishangnian <lishangnian@126.com>
2025-10-11 18:58:20 +0800

    新增贝塞尔曲线实现

--- app/src/main/java/com/enjoy/wanji/vr3D/CarScene.java
+++ app/src/main/java/com/enjoy/wanji/vr3D/CarScene.java
@@ -11,20 +11,21 @@ import com.enjoy.wanji.service.EnjoySocketService;
 
 import org.rajawali3d.Object3D;
 import org.rajawali3d.lights.DirectionalLight;
+import org.rajawali3d.loader.Loader3DSMax;
 import org.rajawali3d.loader.LoaderOBJ;
 import org.rajawali3d.loader.LoaderSTL;
 import org.rajawali3d.loader.ParsingException;
 import org.rajawali3d.materials.Material;
 import org.rajawali3d.materials.methods.DiffuseMethod;
 import org.rajawali3d.materials.methods.SpecularMethod;
+import org.rajawali3d.materials.textures.ATexture;
 import org.rajawali3d.math.vector.Vector3;
 import org.rajawali3d.primitives.Cube;
 import org.rajawali3d.primitives.Line3D;
 import org.rajawali3d.primitives.Plane;
 import org.rajawali3d.renderer.Renderer;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
@@ -37,6 +38,7 @@ public class CarScene extends Renderer {
 
     List<Plane> lineList = new ArrayList<>();
     Line3D leftCurveLine3D, rightCurveLine3D; //曲线
+    Material line3DMaterial;
 //    private Object3D ground;
     float[] colorGrayArr = {0.7216f, 0.7608f, 0.8000f, 1f}; //灰色
     float[] colorLightGrayArr = {0.835f, 0.835f, 0.835f, 1f}; //灰色
@@ -52,7 +54,7 @@ public class CarScene extends Renderer {
 
     float carPositionX = 0, carPositionY = 0.9f, carPositionZ = -1.5f;
     float cameraX = 0, cameraY = 8f, cameraZ = 9f;
-//    float cameraX = 15, cameraY = 1f, cameraZ = 5f;      //测试 侧面视角
+//    float cameraX = 13, cameraY = 1f, cameraZ = 5f;      //测试 侧面视角
 
     float lookAtX = 0, lookAtY = 0, lookAtZ = -6f;
 
@@ -74,7 +76,7 @@ public class CarScene extends Renderer {
         getCurrentScene().addLight(keyLight);
 
         DirectionalLight keyLight1 = new DirectionalLight(-1.5, -1.5f, -2.0f);
-        keyLight1.setPower(0.35f);
+        keyLight1.setPower(0.30f);
         keyLight1.setColor(1.0f, 1.0f, 0.98f);
         getCurrentScene().addLight(keyLight1);
 
@@ -89,9 +91,13 @@ public class CarScene extends Renderer {
         getCurrentScene().addLight(light1);
 
         addLaneLines(); //车道线
+//        bezierLines();  //绘制曲线
 
         float scale = 3.2f;
-//        carModel = initMainCarModel(R.raw.obj_main_car);
+//        float scale = 50f;
+
+//        carModel = initMainCarModel(R.raw.obj_max_car_main);
+//         carModel = initMainCarModel(R.raw.obj_max_car_main);
         carModel = initCenterCarModel(R.raw.stl_main_car, colorAuroraSliverGrayArr);
 //        carModel = initCenterCarModel(R.raw.stl_main_car, colorGrayArr);
         carModel.setScale(scale);
@@ -103,12 +109,24 @@ public class CarScene extends Renderer {
 
         // 设置摄像机位置（固定）  x-右  y-高  z-纵深 靠近观察者为正
         getCurrentCamera().setPosition(cameraX, cameraY, cameraZ);
-//        getCurrentCamera().setPosition(0, 25, 5);  //俯视
         getCurrentCamera().setLookAt(lookAtX, lookAtY, lookAtZ);
-
 
         initModelNPC(); //初始化 背景交通物体
 
+    }
+
+
+
+    private void createInternalFolder(){
+        Log.i("myTag","start create folders");
+        //新建文件夹，用来存放模型文件  Android/data/com.enjoy.wanji
+        File folder = new File(mContext.getExternalFilesDir(null), "models");
+        if (!folder.exists()){
+            folder.mkdirs();
+        }
+        Log.i("myTag","get name: "+folder.getName());
+        Log.i("myTag","get dir absolute path:"+ folder.getAbsolutePath());
+        Log.i("myTag","get dir path:"+ folder.getPath());
     }
 
     /**
@@ -116,29 +134,71 @@ public class CarScene extends Renderer {
      * @param resourceId
      * @return
      */
-    /**
-     *
-
 
     private Object3D initMainCarModel(int resourceId){
+
         Object3D model = null;
         try {
-            InputStream objInputStream = mContext.getAssets().open("model/obj_main_car.obj");
-//            LoaderOBJ loader  = new LoaderOBJ(this, resourceId);
-//            LoaderOBJ loader  = new LoaderOBJ(mContext.getResources(), getTextureManager(), resourceId);
-//            LoaderOBJ loader = new LoaderOBJ(mContext.getResources(),getTextureManager(),objInputStream);
-//            loader.parse();   //解析模型
-//            model = loader.getParsedObject();
+//            LoaderOBJ loader = new LoaderOBJ(mContext.getResources(), mTextureManager,resourceId);
+//            LoaderSTL loader = new LoaderSTL(mContext.getResources(), mTextureManager, resourceId);
+//            InputStream inputStream = mContext.getAssets().open("models/obj_max_car_main.max");
+
+//            File modelFile = new File(mContext.getExternalFilesDir(null), "models/car_main.obj");
+//            File modelFile = new File(mContext.getExternalFilesDir(null), "models/material.mtl");
+            File modelFile = new File(mContext.getExternalFilesDir(null), "models/obj_max_car_main.max");
+//            File modelFile = new File(mContext.getExternalFilesDir(null), "models/texture_pbr_20250901.png");
+            Log.i("myTag","f model exists: "+ modelFile.exists());
 
 
+//            modelFile.setReadable(true, false);
+//            modelFile.setWritable(true);
+//            modelFile.setExecutable(true);
 
-        }catch (ParsingException | IOException pe){
+
+            Loader3DSMax loader = new Loader3DSMax(this, modelFile);
+//                LoaderOBJ loader = new LoaderOBJ(this, modelFile);
+
+
+            loader.parse();   //解析模型
+            model = loader.getParsedObject();
+
+//            Material material = initMaterial();
+//            model.setMaterial(material);
+
+
+            /**
+             *
+
+            // 设置车辆材质（如果没有纹理，使用默认材质）
+            Material material = new Material();
+            material.setColor(colorAuroraSliverGrayArr);
+            material.enableLighting(true);
+            material.setDiffuseMethod(new DiffuseMethod.Lambert());
+
+            // 设置镜面反射 - 实现光滑表面
+            material.setSpecularMethod(new SpecularMethod.Phong(Color.WHITE, 100));
+
+            // 启用颜色影响
+            material.setColorInfluence(0.97f);
+            if (model != null && model.getNumChildren() > 0){
+                for(int i = 0; i < model.getNumChildren(); i++){
+                    Object3D child = model.getChildAt(i);
+                    Log.i("main tag","num child >0 ");
+                    child.setMaterial(material);
+                }
+            }else if (model != null){
+                model.setMaterial(material);
+            }
+             */
+//
+        }catch (ParsingException  pe){   //|ATexture.TextureException
             Log.e("objTag","parsing carObj error:",pe.fillInStackTrace());
+            throw new RuntimeException(pe);
         }
 
         return model;
     }
-     */
+
 
     private Object3D initCenterCarModel(int resourceId, float[] colorARR){
         Object3D model = null;
@@ -174,6 +234,35 @@ public class CarScene extends Renderer {
         return model;
 
     }
+
+
+    private Material initMaterial() throws ATexture.TextureException {
+        String tag = "textureTag";
+        float[] colorArr = {0.8f, 0.8f, 0.8f,1};
+        Material material = new Material();
+        Log.i(tag,"s1 *******");
+//        Texture textureKd = new Texture("kd_png",R.raw.texture_pbr_20250901);
+//        Texture texturePm = new Texture("pm_png",R.raw.texture_pbr_20250901_metallic);//金属贴图
+//        Texture texturePr = new Texture("pr_png",R.raw.texture_pbr_20250901_roughness); //粗糙贴图
+//        Texture textureBump = new Texture("bump_png",R.raw.texture_pbr_20250901_normal); //法线贴图
+//        material.addTexture(textureKd);
+//        material.addTexture(texturePm);
+//        material.addTexture(texturePr);
+//        material.addTexture(textureBump);
+        Log.i(tag,"s2 *******");
+        material.enableLighting(true);
+        material.setColor(colorArr);  //漫反射颜色
+        material.setDiffuseMethod(new DiffuseMethod.Lambert());//漫反射
+        //设置高光颜色  控制高光大小和锐利度值越大越锐利，接近镜面 <50：柔和弥散    50-150：标准高光   >150：锐利高光
+//        material.setSpecularMethod(new SpecularMethod.Phong(Color.WHITE, 30));
+        material.setAmbientColor(colorArr);
+        material.setColorInfluence(0.65f);  // 0.0: 完全使用纹理颜色（忽略材质颜色） 1.0: 完全使用材质颜色（忽略纹理） 0.0-1.0: 两者按比例混合
+
+
+        Log.i(tag,"s3 *******");
+        return material;
+    }
+
 
     /**
      * 初始化机动车模型
@@ -330,24 +419,10 @@ public class CarScene extends Renderer {
 
 
     public void updateLinesMove(double z){
-        //轨迹是曲线转弯，隐藏直线
-        if (DataStorageFromPC.CurveA !=0){
-            for (Plane line: lineList){
-                if (line.isVisible()){
-                    line.setVisible(false);
-                }
-            }
-            drawLines(); //绘制曲线
-            return;
-        }
+        //todo 轨迹是曲线转弯，隐藏直线
 
-        //曲线隐藏
-        if (null != leftCurveLine3D && leftCurveLine3D.isVisible()){
-            leftCurveLine3D.setVisible(false);
-        }
-        if (null != rightCurveLine3D && rightCurveLine3D.isVisible()){
-            rightCurveLine3D.setVisible(false);
-        }
+
+        //todo 曲线隐藏
 
         for (Plane line: lineList){
             Vector3 v = line.getPosition();
@@ -394,94 +469,49 @@ public class CarScene extends Renderer {
     }
 
 
-    static double A, B, C;
-    float thickness = 8f;
-    static float CURVE_OFFSET_X = 1.5f;   //x偏移量
-    final int NUM_PLANES = 60; // 使用的平面点个数
-    final float RANGE = 6.0f; // x轴范围
-
-    double startY = 0.5;
     /**
+     * 计算 贝塞尔曲线
      *
      */
-    private void drawLines(){
-//        Log.i("lineTag","A="+A +", cA="+ DataStorageFromPC.CurveA
-//        +", B="+B+", cB="+DataStorageFromPC.CurveB
-//        +", C="+C +", cC="+DataStorageFromPC.CurveC);
-        if (A == DataStorageFromPC.CurveA
-                && B == DataStorageFromPC.CurveB
-                && C == DataStorageFromPC.CurveC){
-            //与上次曲线一样，直接显示不用再绘制
-            if (leftCurveLine3D != null && !leftCurveLine3D.isVisible()){
-                leftCurveLine3D.setVisible(true);
-            }
-            if (rightCurveLine3D != null && !leftCurveLine3D.isVisible()){
-                rightCurveLine3D.setVisible(true);
-            }
-            return;
+    float thickness = 8;
+    Stack<Vector3> lStack = new Stack<>();
+    Stack<Vector3> rStack = new Stack<>();
+    private void bezierLines(){
+//        double P0x = 0, P0y = 0.209999993443;
+//        double P1x = -0.0399999991059, P1y = 11.6000003815;
+//        double P2x = -0.129999995232, P2y = 21.3199996948;
+//        double P3x = -0.280000001192, P3y = 29.8099994659;
+        double P0x = -0.0900000035763, P0y = 0.0599999986589;
+        double P1x = -0.550000011921, P1y = 11.6099996567;
+        double P2x = -4.76999998093, P2y = 20.5400009155;
+        double P3x = -11.4799995422, P3y = 24.7000007629;
+
+        if (line3DMaterial == null){
+            line3DMaterial = new Material();
+            line3DMaterial.enableLighting(true);
+            line3DMaterial.setColor(0x00bfff);
+            line3DMaterial.setColorInfluence(1);
+//
+//        Texture texture = new Texture("",R.mipmap.bus);
+//
+//        try {
+//            material.addTexture(texture);
+//        } catch (ATexture.TextureException e) {
+//            throw new RuntimeException(e);
+//        }
         }
-        A = DataStorageFromPC.CurveA;
-        B = DataStorageFromPC.CurveB;
-        C = DataStorageFromPC.CurveC;
 
-
-
-        Material material = new Material();
-        material.setColor(0x00bfff);
-//        material.setDiffuseMethod(new DiffuseMethod.Lambert());
-
-        Stack<Vector3> lStack = new Stack<>();
-        Stack<Vector3> rStack = new Stack<>();
-        List<Vector3> lPointsList = new ArrayList<>();
-        List<Vector3> rPointsList = new ArrayList<>();
-        float  x;
-        // 创建多个平面形成二次曲线
-        for (int i = 0; i < NUM_PLANES; i++){
-            // 计算x坐标
-             x = -RANGE + (2 * RANGE * i / (NUM_PLANES - 1));
-
-            // 计算二次函数y值
-            float leftX = x + CURVE_OFFSET_X;   //左平移
-            float rightX = x - CURVE_OFFSET_X;  //右平移
-
-        //   中轴为        float xMedian = B/2A;
-//            Log.i("lineTag","lx ="+ leftX +" ,x="+x+", rx="+ rightX);
-
-            double leftY = -A * leftX * leftX + B * leftX + C;
-            double rightY = -A * rightX * rightX + B * rightX + C;
-
-//            Vector3 lV = new Vector3(x, 0, leftY);
-//            Vector3 rV = new Vector3(x, 0, rightY);
-//            lPointsList.add(lV);
-//            rPointsList.add(rV);
-
-
-            if (B > 0){
-                if (leftY < startY && x > (B/(2*A) - CURVE_OFFSET_X) ){  //只取车头前的轨迹，只要抛物线的右边部分 中线为B/2-CURVE_OFFSET_X
-                    Vector3 lV = new Vector3(x, 0, leftY);
-                    lPointsList.add(lV);
-                }
-                if (rightY < startY && x > (B/(2*A) + CURVE_OFFSET_X)){
-                    Vector3 rV = new Vector3(x, 0, rightY);
-                    rPointsList.add(rV);
-                }
-            }else if (B < 0){
-                if (leftY < startY && x < (B/(2*A) - CURVE_OFFSET_X) ){  //只取车头前的轨迹，只要抛物线的左边部分 中线为B/2-CURVE_OFFSET_X
-                    Vector3 lV = new Vector3(x, 0, leftY);
-                    lPointsList.add(lV);
-                }
-//                Log.i("lineTag",", x="+x+", rightY="+rightY);
-                if (rightY < startY && x < (B/(2*A) + CURVE_OFFSET_X)){
-//                    Log.i("lineTag","x="+x+", rightY="+rightY +"is OK\n");
-                    Vector3 rV = new Vector3(x, 0, rightY);
-                    rPointsList.add(rV);
-                }
-            }
-
-
+        lStack.clear();
+        rStack.clear();
+        for (float t = 0; t <= 1; t += 0.02){
+            double x = getBezier(P0x,P1x,P2x,P3x,t);
+            double y = getBezier(P0y,P1y,P2y,P3y,t);
+            Vector3 vector3L = new Vector3(x-1.7,0, 0-y);
+            Vector3 vector3R = new Vector3(x+1.7,0, 0-y);
+            lStack.add(vector3L);
+            rStack.add(vector3R);
         }
-        lStack.addAll(lPointsList);
-        rStack.addAll(rPointsList);
+
         if (leftCurveLine3D != null){
             getCurrentScene().removeChild(leftCurveLine3D);
         }
@@ -489,15 +519,27 @@ public class CarScene extends Renderer {
             getCurrentScene().removeChild(rightCurveLine3D);
         }
 
-        leftCurveLine3D = new Line3D(lStack,thickness, 0x00bfff); //thickness为线宽，单位是像素
-        rightCurveLine3D = new Line3D(rStack,thickness, 0x00bfff);
-        leftCurveLine3D.setMaterial(material);
-        rightCurveLine3D.setMaterial(material);
-
-        // 将平面添加到场景中
+        leftCurveLine3D = new Line3D(lStack,thickness);
+        rightCurveLine3D = new Line3D(rStack,thickness);
+        leftCurveLine3D.setMaterial(line3DMaterial);
+        rightCurveLine3D.setMaterial(line3DMaterial);
         getCurrentScene().addChild(leftCurveLine3D);
         getCurrentScene().addChild(rightCurveLine3D);
 
     }
 
+    /**
+     *
+     * @param P0
+     * @param P1
+     * @param P2
+     * @param P3
+     * @param t
+     * @return
+     *
+     * B(t) = (1-t)³P₀ + 3(1-t)²tP₁ + 3(1-t)t²P₂ + t³P₃
+     */
+    private double getBezier(double P0, double P1, double P2, double P3, float t){
+        return Math.pow(1-t, 3.0)*P0 + 3*Math.pow(1-t,2)*t*P1 + 3*(1-t)*Math.pow(t,2)*P2 + Math.pow(t,3)*P3;
+    }
 }
